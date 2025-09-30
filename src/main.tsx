15c2e491ed82051c9009133d995efab29e8788f3
Abraham Ayegba <abraham.ayegba@virtualservicesgroup.co.uk>
2025-09-30 11:44:12 +0100

    updated pdf js version

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
 import { pdfjs } from "react-pdf";
-import { Toaster as SonnerToaster } from './components/ui/sonner.tsx';
+import { Toaster as SonnerToaster } from "./components/ui/sonner.tsx";
 import { Toaster as Toaster } from "@/components/ui/toaster";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
-
+pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
