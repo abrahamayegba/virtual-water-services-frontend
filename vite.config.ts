419e33b9d06b2b5b2a07ce9f707437ec03980621
Abraham Ayegba <abraham.ayegba@virtualservicesgroup.co.uk>
2025-10-06 18:26:14 +0100

    fixing dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,20 +5,11 @@ import path from "path";
 export default defineConfig({
   base: "/", // ensures root-relative paths
   build: {
-    outDir: "dist", // output folder is dist (no nested dist)
+    outDir: "dist",
     emptyOutDir: true, // cleans dist before building
   },
   plugins: [react()],
   assetsInclude: ["**/*.pdf"],
-  define: {
-    global: "globalThis",
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
-  },
+  define: { global: "globalThis" },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
 });
