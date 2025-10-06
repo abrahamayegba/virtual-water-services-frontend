3404bfb8c537b6c04a4327c18612d055bdab0332
Abraham Ayegba <abraham.ayegba@virtualservicesgroup.co.uk>
2025-10-06 16:17:07 +0100

    removed nav and footer

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -82,7 +82,7 @@ export default function Dashboard() {
 
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
+      {/* <Navbar /> */}
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 min-h-[725px]">
         <div className="mb-8">
           <h1 className="text-3xl font-bold text-gray-900">
@@ -246,7 +246,7 @@ export default function Dashboard() {
           />
         </div>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
